1ac61ac3704c2ed04f2b54c7c9a46570b67bb7b7
frezer takele <129789211+fretak1@users.noreply.github.com>
2023-05-07 14:52:18 +0300

    Update name.cpp
    advancement in our code

--- name.cpp
+++ name.cpp
@@ -2,32 +2,8 @@
 #include <cstring>
 #include<cctype>
 using namespace std;
-void cheak(){
-  char made[100];
-  int book,count=0;
-  cin>> made;
-  book = strlen(made);
-  for(int i =0; i<book;i++)
-  {
-    bool are = isalpha(made[i]);
-    if(!are)
-    count++;
-  }
-  while(count>0)
-  {
-    cout<<"enter your name again"<<endl;
-    count=0;
-    cin>>made;
-    book =strlen(made);
-    for(int i=0;i<book;i++)
-    {
-      bool are =isalpha(made[i]);
-      if(!are)
-      count++;
-    }
 
-  }
-}
+  
 void passengers();
 int main()
 {
@@ -42,9 +18,57 @@ int main()
   {
     char firstname[25],lastname[25],departure[25],arrival[25],email[25];
     cout<<" please,enter your first name"<<endl;
+    // the loop used to check if the input includes other charcter without leters
+  int book,count=0;
+  cin>> firstname;
+  book = strlen(firstname);
+  for(int i =0; i<book;i++)
+  {
+    bool are = isalpha(firstname[i]);
+    if(!are)
+    count++;
+  }
+  while(count>0)
+  {
+    cout<<"enter your name again"<<endl;
+    count=0;
     cin>>firstname;
+    book =strlen(firstname);
+    for(int i=0;i<book;i++)
+    {
+      bool are =isalpha(firstname[i]);
+      if(!are)
+      count++;
+    }
+
+  }
     cout<<"enter your last name "<<endl;
+     // the loop used to check if the input includes other charcter without leters   
+  count=0;
+  cin>> lastname;
+  book = strlen(lastname);
+  for(int i =0; i<book;i++)
+  {
+    bool are = isalpha(lastname[i]);
+    if(!are)
+    count++;
+  }
+  while(count>0)
+  {
+    cout<<"enter your name again"<<endl;
+    count=0;
     cin>>lastname;
+    book =strlen(lastname);
+    for(int i=0;i<book;i++)
+    {
+      bool are =isalpha(lastname[i]);
+      if(!are)
+      count++;
+    }
+
+  } 
+   
+   
     int age;
     char phonenumber[20];
     cout<<"enter your age number "<<endl;
